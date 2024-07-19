72554b7ffbc2be3cf5eb71c45a47788edcf5a1c8
Your Name <yt.atchyut@gmail.com>
2024-07-19 23:43:55 +0530

    final commit

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
     getHello(): string {
-        return 'Hello! from Todo rest API!';
+        return 'Hello! from Todo rest API! Go to <a href="https://simple-todo-two-coral.vercel.app/swagger">My Swagger UI<a>';
     }
 }
